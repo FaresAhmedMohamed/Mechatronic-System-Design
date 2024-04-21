b423f50713ee7e91c23442956724d35422a918bc
FaresAhmedMohamed <19P6135@eng.asu.edu.eg>
2024-04-21 02:30:45 +0200

    4 leds 1.1

--- pythonProject/MSDTest/ObjectDetection.py
+++ pythonProject/MSDTest/ObjectDetection.py
@@ -4,33 +4,34 @@ import numpy
 
 arduino = SerialObject()
 #cap = cv2.VideoCapture(0)
-img = cv2.imread("../TestImg/cubel.png") #remove when using cam
-def empty(a):
-    pass
+img = cv2.imread("../TestImg/4objects.png") #remove when using cam
+
 
 def getContours(img,imgContour):
     squares=0
     circles=0
     prevarea=0
+    px=0
+    py=0
     contours,hierarchy = cv2.findContours(img,cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
     for cnt in contours:
         area = cv2.contourArea(cnt)
-        if area==0:
+        if area==0 or area<1000:
             continue
-        differ=abs(area-prevarea)/area
-        if differ<0.5:
+        peri = cv2.arcLength(cnt, True)
+        approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
+        objCor = len(approx)
+        x, y, w, h = cv2.boundingRect(approx)
+        differa=abs(area-prevarea)/area
+        differx=abs(x-px)
+        differy=abs(y-py)
+        if differx<50 and differy<50 and differa<0.5:
             continue
         prevarea = area
-        #print(area)
-        if area>1000:
+        px=x
+        py=y
+        if x < 600 and y < 600:
             cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)
-            peri = cv2.arcLength(cnt,True)
-            #print(peri)
-            approx = cv2.approxPolyDP(cnt,0.02*peri,True)
-            #print(len(approx))
-            objCor = len(approx)
-            x, y, w, h = cv2.boundingRect(approx)
-
             if objCor == 4:
                 aspRatio = w/float(h)
                 if aspRatio >0.93 and aspRatio <1.07:
@@ -47,6 +48,11 @@ def getContours(img,imgContour):
     idobj = [squares, circles]
     return idobj
 
+def most_frequent(List):
+    unique, counts = numpy.unique(List, return_counts=True)
+    index = numpy.argmax(counts)
+    return unique[index]
+
 #cv2.namedWindow("Parameters")
 #cv2.resizeWindow("Parameters",640,240)
 #cv2.createTrackbar("Threshold1","Parameters",130,255,empty)
@@ -55,41 +61,105 @@ def getContours(img,imgContour):
 while True:
     go = arduino.getData()
     if go[0]=='9':
-        squarevals=[]
-        circlevals=[]
-        framesread=0
-        while framesread<100:
-            # success, img = cap.read() #add when using cam
-            imgContour = img.copy()
-            imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            imgBlur = cv2.GaussianBlur(imgGray, (11, 11), 0)
-            #threshold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
-            #threshold2 = cv2.getTrackbarPos("Threshold2", "Parameters")
-            threshold1 = 130
-            threshold2 = 20
-            imgCanny = cv2.Canny(imgBlur, threshold1, threshold2)
-            kernel = numpy.ones((5, 5), )
-            imgDialation = cv2.dilate(imgCanny, kernel, iterations=1)
-            idobj = getContours(imgCanny, imgContour)
-            squarevals.append(idobj[0])
-            circlevals.append(idobj[1])
-            cv2.imshow("Image", imgContour)
-            cv2.imshow("ImageC", imgCanny)
-            cv2.resizeWindow("Image", 1000, 1000)
-            cv2.waitKey(1)
-            framesread = framesread+1
-        cv2.destroyAllWindows()
-        maxsqval = max(squarevals)
-        maxcircval = max(circlevals)
-        if maxsqval==2 and maxcircval==0:
-            arduino.sendData([1])
-            img = cv2.imread("../TestImg/circleb.png")
-        elif maxsqval==1 and maxcircval==0:
-            arduino.sendData([2])
-            img = cv2.imread("../TestImg/circlel.png")
-        elif maxsqval==0 and maxcircval==2:
-            arduino.sendData([3])
-        elif maxsqval==0 and maxcircval==1:
-            arduino.sendData([4])
-            img = cv2.imread("../TestImg/cubeb.png")
+        counter=0
+        arduinolist=[]
+        while counter<4:
+            squarevals=[]
+            circlevals=[]
+            framesread=0
+            while framesread<100:
+                # success, img = cap.read() #add when using cam
+                imgContour = img.copy()
+                imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                imgBlur = cv2.GaussianBlur(imgGray, (11, 11), 0)
+                #threshold1 = cv2.getTrackbarPos("Threshold1", "Parameters")
+                #threshold2 = cv2.getTrackbarPos("Threshold2", "Parameters")
+                threshold1 = 130
+                threshold2 = 20
+                imgCanny = cv2.Canny(imgBlur, threshold1, threshold2)
+                kernel = numpy.ones((3, 3), )
+                #imgDilation = cv2.dilate(imgCanny, kernel, iterations=1)
+                #imgErosion = cv2.erode(imgCanny, kernel, iterations=1)
+                idobj = getContours(imgCanny, imgContour)
+                squarevals.append(idobj[0])
+                circlevals.append(idobj[1])
+                cv2.imshow("Image", imgContour)
+                #cv2.imshow("ImageC", imgCanny)
+                cv2.resizeWindow("Image", 1200, 1200)
+                cv2.waitKey(1)
+                framesread = framesread+1
+            cv2.destroyAllWindows()
+            sqval = most_frequent(squarevals)
+            circval = most_frequent(circlevals)
+            if sqval==2 and circval==0:
+                arduinolist.append("sb")
+            elif sqval==1 and circval==0:
+                arduinolist.append("sl")
+            elif sqval==0 and circval==2:
+                arduinolist.append("cb")
+            elif sqval==0 and circval==1:
+                arduinolist.append("cl")
+            img = cv2.rotate(img,cv2.ROTATE_90_CLOCKWISE)
 
+            counter+=1
+            arduino.sendData([counter])
+            while True:
+                go = arduino.getData()
+                if go[0] == '8':
+                    go[0]='0'
+                    break
+    i=0
+    while i<len(arduinolist):
+        print(arduinolist[i])
+        i+=1
+    currentposition=len(arduinolist)-1
+    final=[]
+    if arduinolist[currentposition] in ("sl","cl"):
+        j = 0
+        while j<len(arduinolist):
+            if arduinolist[(currentposition+j+1)%len(arduinolist)] in ("sb","cb"):
+                final.append((currentposition+j+1)%len(arduinolist))
+                final.append("ccw")
+                currentposition = (currentposition+j+1)%len(arduinolist)
+                break
+            elif arduinolist[(currentposition-(j+1))%len(arduinolist)] in ("sb","cb"):
+                final.append((currentposition-(j+1))%len(arduinolist))
+                final.append("cw")
+                currentposition = (currentposition-(j+1))%len(arduinolist)
+                break
+            j+=1
+    j=0
+    passed=[]
+    while j<len(arduinolist)-1:
+        i = currentposition
+        objtype=arduinolist[currentposition][0]
+        passed.append(i)
+        turns = 1
+        direction = "ccw"
+        while i<currentposition+len(arduinolist):
+            if (i+1)%len(arduinolist) in passed:
+                i += 1
+                turns += 1
+                continue
+            nextobject = arduinolist[((i+1) % len(arduinolist))]
+            if ((nextobject == "sl" and objtype == "s") or (nextobject == "cl" and objtype == "c")) and j%2==0:
+                if turns>(len(arduinolist)/2):
+                    direction="cw"
+                currentposition = (i+1) % len(arduinolist)
+                final.append((i+1) % len(arduinolist))
+                final.append(direction)
+                break
+            elif (nextobject in ("sb","cb")) and j%2==1:
+                if turns>(len(arduinolist)/2):
+                    direction="cw"
+                currentposition = (i+1) % len(arduinolist)
+                final.append((i+1) % len(arduinolist))
+                final.append(direction)
+                break
+            i += 1
+            turns += 1
+        j+=1
+    i = 0
+    while i < len(final):
+        print(final[i])
+        i += 1
---
+++ robotarmcode2/robotarmcode2.ino
@@ -0,0 +1,48 @@
+#include <cvzone.h>
+
+SerialData serialData(1,1);
+int objects[8];
+int signalout[2];
+int signalin[1];
+
+void setup() {
+serialData.begin();
+pinMode(13,INPUT);
+pinMode(12,OUTPUT);
+pinMode(11,OUTPUT);
+pinMode(10,OUTPUT);
+pinMode(9,OUTPUT);
+pinMode(8,OUTPUT);
+pinMode(7,OUTPUT);
+digitalWrite(12,HIGH);
+}
+
+void loop() {
+if (digitalRead(13)==1)
+{
+  signalout[0]=9;
+  serialData.Send(signalout);
+  int counter=0;
+  int position=1;
+  signalout[0]=8;
+  while(counter<3)
+    {
+    while(1)
+    {
+      serialData.Get(signalin);
+      if(signalin[0]==1+counter)
+        signalin[0]=0;
+        {break;}
+    }
+    delay(3000);
+    digitalWrite(12-position+1,LOW);
+    digitalWrite(12-position,HIGH);
+    serialData.Send(signalout);
+    position++;
+    counter++;
+    }
+    serialData.Send(signalout);
+}
+}
+
+
